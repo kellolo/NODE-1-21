42749cc305182856daf14cfdebaa3f52ed243808
Vitaly <vitalii.a.sh@mail.ru>
2021-06-06 12:14:53 +0300

    lesson 7

---
+++ STUDENTS/Vitaly Shishov/lesson7/ToDoList/api/src/db/dataBaseConfig.js
@@ -0,0 +1,10 @@
+const DB_CONFIG = {
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "node-todo",
+    tableUsers: "users",
+    tableTodos: "todo_list"
+}
+
+module.exports = DB_CONFIG;
